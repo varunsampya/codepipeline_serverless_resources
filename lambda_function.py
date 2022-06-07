60cd581f21b611b2839ffb23e9d36af14eb6de14
varunsampya <47588507+varunsampya@users.noreply.github.com>
2022-06-07 20:33:33 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'AWS CodePipeline demo!',
+        'output': 'AWS Guru!',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
